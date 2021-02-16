c6f5946adb1b7b0e321635b3e1e4151cf6f19dd3
David Tresco Emes <emes.davidt@gmail.com>
2021-02-16 17:18:52 +0000

    Added a function which does PSA and plots the CEAC

--- A simple ICER function that reads from CSV.R
+++ A simple ICER function that reads from CSV.R
@@ -2,7 +2,7 @@ setwd("C:/Users/tresc/Dropbox/LSHTM")
 
 ICER_csv <- function(inputdata){
   setwd("C:/Users/tresc/Dropbox/LSHTM")
-  inputdata <- read.csv(inputdata)
+  inputdata <- read.csv("inputdata.csv")
   inputdata <- as.data.frame(inputdata)
   costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
   total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
@@ -23,7 +23,48 @@ ICER_csv("inputdata.csv")
 ICER_csv("pessimistic inputs.csv")
 ICER_csv("optimistic inputs.csv")
 
-##New branch
-#Lorem ipsum dolor sit amet
+################################################################################
+#Try randomising part of the function, and storing simulation results
 
-#fhreuiheruifherufirehi
+ICER_rand <- function(inputdata){
+  setwd("C:/Users/tresc/Dropbox/LSHTM")
+  inputdata <- read.csv("inputdata.csv")
+  inputdata <- as.data.frame(inputdata)
+  
+  #replace intervention cost with a random draw
+  inputdata$state_costs_intervention[1] <- rnorm(1,500,100)
+  
+  costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
+  total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
+  QALYs_intervention <- inputdata$state_distribution_intervention * inputdata$state_QALYs
+  total_QALYs_intervention <- QALYs_intervention[1]+QALYs_intervention[2]+QALYs_intervention[3]
+  
+  costs_control <- inputdata$state_distribution_control * inputdata$state_costs_control
+  total_cost_control <- costs_control[1]+costs_control[2]+costs_control[3]
+  QALYs_control <- inputdata$state_distribution_control * inputdata$state_QALYs
+  total_QALYs_control <- QALYs_control[1]+QALYs_control[2]+QALYs_control[3]
+  
+  ICER <- (total_cost_intervention - total_cost_control) / (total_QALYs_intervention - total_QALYs_control)
+  
+  return(ICER)
+}
+
+ICER_Vector <- c(rep(0,10000))
+
+for(i in 1:10000){
+  ICER_Vector[i] <- ICER_rand("inputdata.csv")
+  rm(i)
+}
+
+hist(ICER_Vector)
+
+density <- density(ICER_Vector)
+
+plot(density)
+
+CEAC <- ecdf(ICER_Vector)
+
+plot(CEAC,
+     xlab = 'Willingness to Pay to Avert an Additional DALY',
+     ylab = "Portion of Interventions Cost-Effective",
+     main = 'Cost Effectiveness Acceptability Curve')
