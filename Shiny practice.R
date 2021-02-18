72aebd6e269b2e8c958e2c3bf7fc02e1e218bbd0
David Tresco Emes <emes.davidt@gmail.com>
2021-02-18 12:40:50 +0000

    Added a few tabs to give a deterministic ICER, randomly drawn ICER, CEAC and a probabilistic sensitivity analysis with a randomised WTP

--- Shiny practice.R
+++ Shiny practice.R
@@ -7,18 +7,123 @@ library(tidyr)
 library(data.table)
 library("shinydashboard")
 
+
 ui <- dashboardPage(
-  dashboardHeader(title = "Page Title"),
-  dashboardSidebar(),
+  dashboardHeader(title = "Cost Effectiveness"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Deterministic", tabName = "determ", icon = icon("balance-scale")),
+      menuItem("Single Draw", tabName = "rand_draw", icon = icon("dice")),
+      menuItem("Montecarlo Microimulation", tabName = "CEAC", icon = icon("chart-area")),
+      menuItem("Randomised Willingness to Pay", tabName = "portion", icon = icon("dice-d20"))
+    )
+  ),
   dashboardBody(
-    box(plotOutput("plot name"), width = 8)
+    tabItems(
+      tabItem("rand_draw",
+              box(
+                title = "Incremental Cost-Effectiveness Ratio (Single Draw from a Distribution)",
+                background = "fuchsia",
+                width = 4,
+                textOutput("ICER")
+              ),
+              box(
+                numericInput("int_cost", em("Expected Healthcare Cost with Intervention:"), 500, min = 0, max = 10000), width = 4
+              ),
+              box(
+                numericInput("bau_cost", em("Expected Healthcare Cost without Intervention:"), 100, min = 0, max = 10000), width = 4
+              )       
+      ),
+      tabItem("CEAC",
+              box(
+                title = "Cost-Effectiveness Acceptability Curve",
+                background = "teal",
+                width = 8,
+                plotOutput("CEAC")
+              ),
+              box(
+                numericInput("CEAC_int_cost", em("Healthcare Cost with Intervention:", 500, min = 0, max = 10000), width = 4)
+              )
+      ),
+      tabItem("determ",
+              box(
+                title = "Incremental Cost-Effectiveness Ratio (Deterministic)",
+                background = "red",
+                width = 4,
+                textOutput("ICER_determ")
+              ),
+              box(
+                numericInput("determ_int_cost", em("Healthcare Cost with Intervention:", 500, min = 0, max = 10000), width = 4)
+              ),
+              box(
+                numericInput("determ_bau_cost", em("Healthcare Cost without Intervention:", 500, min = 0, max = 10000), width = 4)
+              )),
+      tabItem("portion",
+              box(
+                title = "Portion of Interventions that are Cost Effective",
+                background = "yellow",
+                width = 4,
+                textOutput("portion")
+              ))
+    )
+
   )
   
 )
 
 server <- function(input, output) {
-  output$our_plot <- renderPlot({
-    plot(iris$Sepal.Length, iris$Petal.Length)
+  ICER_rand <- function(inputdata){
+    setwd("C:/Users/tresc/Dropbox/LSHTM")
+    inputdata <- read.csv(inputdata)
+    inputdata <- as.data.frame(inputdata)
+    
+    #replace intervention cost with a random draw
+    inputdata$state_costs_intervention[1] <- rnorm(1,input$int_cost,100)
+    
+    #replace BAU cost with a random draw
+    inputdata$state_costs_control[1] <- rnorm(1,input$bau_cost,100)
+    
+    costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
+    total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
+    QALYs_intervention <- inputdata$state_distribution_intervention * inputdata$state_QALYs
+    total_QALYs_intervention <- QALYs_intervention[1]+QALYs_intervention[2]+QALYs_intervention[3]
+    
+    costs_control <- inputdata$state_distribution_control * inputdata$state_costs_control
+    total_cost_control <- costs_control[1]+costs_control[2]+costs_control[3]
+    QALYs_control <- inputdata$state_distribution_control * inputdata$state_QALYs
+    total_QALYs_control <- QALYs_control[1]+QALYs_control[2]+QALYs_control[3]
+    
+    ICER <- (total_cost_intervention - total_cost_control) / (total_QALYs_intervention - total_QALYs_control)
+    
+    return(ICER)
+  } 
+  
+  ICER_determ <- function(inputdata_determ){
+    
+  }
+  
+  CEAC <- function(inputdata_CEAC){
+    
+  }
+  
+  Portion <- function(inputdata_portion){
+    
+  }
+  
+  output$ICER <- renderText({
+    ICER_rand("inputdata.csv")
+    })
+  
+  output$ICER_determ <- renderText ({
+    ICER_determ("inputdata.csv")
+  })
+  
+  output$CEAC <- renderPlot ({
+    CEAC("inputdata.csv")
+  })
+  
+  output$portion <- renderText ({
+    portion("inputdata.csv")
   })
 }
 
