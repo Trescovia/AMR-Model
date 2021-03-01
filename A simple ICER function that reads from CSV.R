b83c7ba01381ee8b8285eae1bb8672630f7c7773
David Tresco Emes <emes.davidt@gmail.com>
2021-03-01 11:26:41 +0000

    latest version, incl. ARIMA projections and cost-effective plane

--- A simple ICER function that reads from CSV.R
+++ A simple ICER function that reads from CSV.R
@@ -146,7 +146,7 @@ ICER_rand_2 <- function(inputdata){
   return(ICER)
 }
 
-################################################################################
+library(tidyverse)
 
 cost_effective <- function(inputdata){
   
@@ -154,19 +154,17 @@ cost_effective <- function(inputdata){
   inputdata <- read.csv(inputdata)
   inputdata <- as.data.frame(inputdata)
   
-  BAU_Cost_Vector <-c(rep(NA,10000))
-  Int_Cost_vector <-c(rep(NA,10000))
-  Sick_QALY_Vector <- c(rep(NA,10000))
-  Helth_QALY_Vector <- c(rep(NA,10000))
+  BAU_Cost_Vector <-c(rep(NA,100000))
+  Int_Cost_vector <-c(rep(NA,100000))
+  Sick_QALY_Vector <- c(rep(NA,100000))
+  Helth_QALY_Vector <- c(rep(NA,100000))
   
   CEspace <- as.data.frame(cbind(BAU_Cost_Vector, Int_Cost_vector, Sick_QALY_Vector, Helth_QALY_Vector))
   
-  for(i in 1:10000){
-    CET <- rnorm(1,10000,2500)
-    inputdata$state_costs_intervention[1] <- rnorm(1,500,100)
-    inputdata$state_costs_control[1] <- rnorm(1,100,100)
-    inputdata$state_QALYs[1] <- runif(1,0.8,1)
-    inputdata$state_QALYs[2] <- runif(1,0.3,0.7)
+  for(i in 1:100000){
+    CET <- 10000
+    inputdata$state_costs_intervention[1] <- runif(1,0,1000)
+    inputdata$state_costs_control[1] <- rnorm(1,0,1000)
     
     costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
     total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
@@ -180,7 +178,7 @@ cost_effective <- function(inputdata){
     
     ICER <- (total_cost_intervention - total_cost_control) / (total_QALYs_intervention - total_QALYs_control)
     
-    if(abs(CET - ICER) < 250){
+    if(CET >= ICER){
       CEspace$BAU_Cost_Vector[i] <- inputdata$state_costs_control[1]
       CEspace$Int_Cost_vector[i] <- inputdata$state_costs_intervention[1]
       CEspace$Helth_QALY_Vector[i] <- inputdata$state_QALYs[1]
@@ -189,18 +187,146 @@ cost_effective <- function(inputdata){
     rm(i)
   }
   
-  Cost_Effective_Points <- plot(CEspace$BAU_Cost_Vector, CEspace$Int_Cost_vector,
-                                abline(lm(BAU_Cost_Vector ~ Int_Cost_vector, data = CEspace)),
-                                xlab = "Healthcare Cost without Intervention",
-                                ylab = "Healthcare Cost with Intervention",)
+  Cost_Effective_Points <- ggplot(CEspace, aes(x=BAU_Cost_Vector, y=Int_Cost_vector))+
+  geom_point()+
+  coord_cartesian(xlim = c(0,1000), ylim = c(0,1000))+
+  xlab("Health Cost without Intervention")+
+  ylab("Health Cost with Intervention")+
+  ggtitle("Cost-Effective Plane")
   
   return(Cost_Effective_Points)
 }
 
 cost_effective("inputdata.csv")
 
-a <- c(1,2,3,4,5,6,7,8,9)
-b <- c(9,8,7,6,5,4,3,2,1)
+################################################################################
+#Tornado plots (create a different dataset for each ICER)
 
-plot(a,b,
-     abline(lm(a~b)))
+ICER_tornado <- function(inputdata){
+
+  costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
+  total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
+  QALYs_intervention <- inputdata$state_distribution_intervention * inputdata$state_QALYs
+  total_QALYs_intervention <- QALYs_intervention[1]+QALYs_intervention[2]+QALYs_intervention[3]
+  
+  costs_control <- inputdata$state_distribution_control * inputdata$state_costs_control
+  total_cost_control <- costs_control[1]+costs_control[2]+costs_control[3]
+  QALYs_control <- inputdata$state_distribution_control * inputdata$state_QALYs
+  total_QALYs_control <- QALYs_control[1]+QALYs_control[2]+QALYs_control[3]
+  
+  ICER <- (total_cost_intervention - total_cost_control) / (total_QALYs_intervention - total_QALYs_control)
+  
+  return(ICER)
+}
+
+
+ICER_compare <- ICER_tornado(read.csv("inputdata.csv"))
+
+data_BAUcost_max <- read.csv("inputdata.csv") 
+data_BAUcost_max$state_costs_control[1] <- 500
+ICER_BAUcost_max <- ICER_tornado(data_BAUcost_max)
+BAUcost_max <- ICER_BAUcost_max - ICER_compare
+
+data_BAUcost_min <- read.csv("inputdata.csv") 
+data_BAUcost_min$state_costs_control[1] <- 0
+ICER_BAUcost_min <- ICER_tornado(data_BAUcost_min)
+BAUcost_min <- ICER_BAUcost_min - ICER_compare
+
+data_intcost_max <- read.csv("inputdata.csv") 
+data_intcost_max$state_costs_intervention[1] <- 1000
+ICER_intcost_max <- ICER_tornado(data_intcost_max)
+intcost_max <- ICER_intcost_max - ICER_compare
+
+data_intcost_min <- read.csv("inputdata.csv") 
+data_intcost_min$state_costs_intervention[1] <- 100
+ICER_intcost_min <- ICER_tornado(data_intcost_min)
+intcost_min <- ICER_intcost_min - ICER_compare
+
+data_healthyqol_max <- read.csv("inputdata.csv")
+data_healthyqol_max$state_QALYs[1] <- 1
+ICER_healthyqol_max <- ICER_tornado(data_healthyqol_max)
+healthyqol_max <- ICER_healthyqol_max - ICER_compare
+
+data_healthyqol_min <- read.csv("inputdata.csv") 
+data_healthyqol_min$state_QALYs[1] <- 0.7
+ICER_healthyqol_min <- ICER_tornado(data_healthyqol_max)
+healthyqol_min <- ICER_healthyqol_min - ICER_compare
+
+data_sickqol_max <- read.csv("inputdata.csv") 
+data_sickqol_max$state_QALYs[2] <- 0.8
+ICER_sickqol_max <- ICER_tornado(data_sickqol_max)
+sickqol_max <- ICER_sickqol_max - ICER_compare
+
+data_sickqol_min <- read.csv("inputdata.csv") 
+data_sickqol_min$state_QALYs[2] <- 0.3
+ICER_sickqol_min <- ICER_tornado(data_sickqol_max) 
+sickqol_min <- ICER_sickqol_min - ICER_compare
+
+
+
+
+
+
+
+
+
+
+
+
+Tornado <- function(inputdata){
+  setwd("C:/Users/tresc/Dropbox/LSHTM")
+  inputdata <- read.csv(inputdata)
+  inputdata <- as.data.frame(inputdata)
+  
+  ICER_compare <- ICER_tornado(inputdata)
+  
+  data_BAUcost_max <- read.csv(inputdata) 
+  data_BAUcost_max$state_costs_control[1] <- 500
+  ICER_BAUcost_max <- ICER_tornado(data_BAUcost_max)
+  BAUcost_max <- ICER_BAUcost_max - ICER_compare
+  
+  data_BAUcost_min <- read.csv(inputdata) 
+  data_BAUcost_min$state_costs_control[1] <- 0
+  ICER_BAUcost_min <- ICER_tornado(data_BAUcost_min)
+  BAUcost_min <- ICER_BAUcost_min - ICER_compare
+  
+  data_intcost_max <- read.csv(inputdata) 
+  data_intcost_max$state_costs_control[1] <- 1000
+  ICER_intcost_max <- ICER_tornado(data_intcost_max)
+  intcost_max <- ICER_intcost_max - ICER_compare
+  
+  data_intcost_min <- read.csv(inputdata) 
+  data_intcost_min$state_costs_control[1] <- 100
+  ICER_intcost_min <- ICER_tornado(data_intcost_min)
+  intcost_min <- ICER_intcost_min - ICER_compare
+  
+  data_healthyqol_max <- read.csv(inputdata)
+  data_healthyqol_max$state_QALYs[1] <- 1
+  ICER_healthyqol_max <- ICER_tornado(data_healthyqol_max)
+  healthyqol_max <- ICER_healthyqol_max - ICER_compare
+
+  data_healthyqol_min <- read.csv(inputdata) 
+  data_healthyqol_min$state_QALYs[1] <- 0.7
+  ICER_healthyqol_min <- ICER_tornado(data_healthyqol_max)
+  healthyqol_min <- ICER_healthyqol_min - ICER_compare
+  
+  data_sickqol_max <- read.csv(inputdata) 
+  data_sickqol_max$state_QALYs[2] <- 0.8
+  ICER_sickqol_max <- ICER_tornado(data_sickqol_max)
+  sickqol_max <- ICER_sickqol_max - ICER_compare
+  
+  data_sickqol_min <- read.csv(inputdata) 
+  data_sickqol_min$state_QALYs[2] <- 0.3
+  ICER_sickqol_min <- ICER_tornado(data_sickqol_max) 
+  sickqol_min <- ICER_sickqol_min - ICER_compare
+  
+  tornado_plot <- ggplot(data.frame(y=c(0,5)), aes(y))+
+    geom_segment(aes(x = sickqol_min, y = 1, xend = sickqol_max, yend = 1, colour = "blue"))+
+    geom_segment(aes(x = healthyqol_min, y = 2, xend = healthyqol_max, yend = 2, colour = "green"))+
+    geom_segment(aes(x = intcost_min, y = 3, xend = intcost_max, yend = 3, colour = "pink"))+
+    geom_segment(aes(x = BAUcost_min, y = 4, xend = BAUcost_min, yend = BAUcost_max, colour = "orange"))
+    
+  return(tornado_plot)
+}
+
+Tornado("inputdata.csv")
--- Shiny practice.R
+++ Shiny practice.R
@@ -9,6 +9,7 @@ library("shinydashboard")
 library("rsconnect")
 library("PKI")
 library("packrat")
+library(tidyverse)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Cost Effectiveness"),
@@ -16,8 +17,10 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Deterministic", tabName = "determ", icon = icon("balance-scale")),
       menuItem("Single Draw", tabName = "rand_draw", icon = icon("dice")),
-      menuItem("Montecarlo Microimulation", tabName = "CEAC", icon = icon("chart-area")),
-      menuItem("Randomised Willingness to Pay", tabName = "portion", icon = icon("dice-d20"))
+      menuItem("Montecarlo MicroSimulation", tabName = "CEAC", icon = icon("chart-area")),
+      menuItem("Randomised Willingness to Pay", tabName = "portion", icon = icon("dice-d20")),
+      menuItem("The Cost-Effective Plane", tabName = "plane", icon = icon("plane")),
+      menuItem("Tornado Plot", tabName = "tornado", icon = icon("poo-storm"))
     )
   ),
   dashboardBody(
@@ -96,7 +99,86 @@ ui <- dashboardPage(
                             "Upload Parameter Spreadsheet",
                             multiple = F,
                             accept= c("csv", "comma-separated-values", ".csv")))
-              )
+              ),
+      tabItem("plane",
+              box(
+                title = "For What Parameter Values Is the Intervention Cost-Effective?",
+                background = "purple",
+                width = 8,
+                plotOutput("plane")
+              ),
+              box(fileInput("plane_file",
+                            "Upload Parameter Spreadsheet",
+                            multiple = F,
+                            accept = c("csv", "comma-separated-values", ".csv")), width = 4),
+              box(selectInput("xparam", "X-Parameter:",
+                              c("BAU_Cost_Vector", "Int_Cost_vector", "Helth_QALY_Vector", "Sick_QALY_Vector")), width = 6),
+              box(selectInput("yparam", "Y-Parameter:",
+                              c("Int_Cost_vector", "BAU_Cost_Vector", "Helth_QALY_Vector", "Sick_QALY_Vector")), width = 6),
+              box(numericInput("QHmin",
+                               em("QoL in Healthy State - Minimum Plausible Value"),
+                               0.9, min = 0, max = 1), width = 3),
+              box(numericInput("QHmax",
+                               em("QoL in Healthy State - Maximum Plausible Value"),
+                               0.9, min = 0, max = 1), width = 3),
+              box(numericInput("QSmin",
+                               em("QoL in Sick State - Minimum Plausible Value"),
+                               0.6, min = 0, max = 1), width = 3),
+              box(numericInput("QSmax",
+                               em("QoL in Sick State - Maximum Plausible Value"),
+                               0.6, min = 0, max = 1), width = 3),
+              box(numericInput("CImin",
+                               em("Healthcare Cost with Intervention - Minimum Plausible Value"),
+                               0, min = 0, max = 10000), width = 3),
+              box(numericInput("CImax",
+                               em("Healthcare Cost with Intervention - Maximum Plausible Value"),
+                               1000, min = 0, max = 10000), width = 3),
+              box(numericInput("CCmin",
+                               em("Healthcare Cost without Intervention - Minimum Plausible Value"),
+                               0, min = 0, max = 10000), width = 3),
+              box(numericInput("CCmax",
+                               em("Healthcare Cost without Intervention - Maximum Plausible Value"),
+                               1000, min = 0, max = 10000), width = 3)
+              
+              ),
+      tabItem("tornado",
+              box(
+                title = "Sensitivity of ICER to Each Parameter",
+                background = "green",
+                width = 12,
+                plotOutput("Tornado")
+              ),
+              box(fileInput("tornado_file",
+                            "Upload Parameter Spreadsheet",
+                            multiple = F,
+                            accept = c("csv", "comma-separated-values", ".csv")), width = 12),
+              box(numericInput("t_QHmin",
+                               em("QoL in Healthy State - Minimum Plausible Value"),
+                               0.7, min = 0, max = 1), width = 3),
+              box(numericInput("t_QHmax",
+                               em("QoL in Healthy State - Maximum Plausible Value"),
+                               1, min = 0, max = 1), width = 3),
+              box(numericInput("t_QSmin",
+                               em("QoL in Sick State - Minimum Plausible Value"),
+                               0.3, min = 0, max = 1), width = 3),
+              box(numericInput("t_QSmax",
+                               em("QoL in Sick State - Maximum Plausible Value"),
+                               0.8, min = 0, max = 1), width = 3),
+              box(numericInput("t_CImin",
+                               em("Healthcare Cost with Intervention - Minimum Plausible Value"),
+                               0, min = 0, max = 10000), width = 3),
+              box(numericInput("t_CImax",
+                               em("Healthcare Cost with Intervention - Maximum Plausible Value"),
+                               1000, min = 0, max = 10000), width = 3),
+              box(numericInput("t_CCmin",
+                               em("Healthcare Cost without Intervention - Minimum Plausible Value"),
+                               0, min = 0, max = 10000), width = 3),
+              box(numericInput("t_CCmax",
+                               em("Healthcare Cost without Intervention - Maximum Plausible Value"),
+                               1000, min = 0, max = 10000), width = 3)
+              
+        
+      )
     )
 
   )
@@ -262,21 +344,85 @@ server <- function(input, output) {
 
   }
   
+  cost_effective <- function(inputdata){
+    
+    inputfile <- input$plane_file
+    
+    inputdata <- read.csv(inputfile$datapath)
+    
+    inputdata <- as.data.frame(inputdata)
+    
+    BAU_Cost_Vector <-c(rep(NA,30000))
+    Int_Cost_vector <-c(rep(NA,30000))
+    Sick_QALY_Vector <- c(rep(NA,30000))
+    Helth_QALY_Vector <- c(rep(NA,30000))
+    
+    CEspace <- as.data.frame(cbind(BAU_Cost_Vector, Int_Cost_vector, Sick_QALY_Vector, Helth_QALY_Vector))
+    
+    for(i in 1:30000){
+      CET <- 10000
+      inputdata$state_costs_intervention[1] <- runif(1,input$CImin,input$CImax)
+      inputdata$state_costs_control[1] <- runif(1,input$CCmin,input$CCmax)
+      inputdata$state_QALYs[1] <- runif(1,input$QHmin,input$QHmax)
+      inputdata$state_QALYs[2] <- runif(1,input$QSmin,input$QSmax)
+      
+      costs_intervention <- inputdata$state_distribution_intervention * inputdata$state_costs_intervention
+      total_cost_intervention <- costs_intervention[1]+costs_intervention[2]+costs_intervention[3]
+      QALYs_intervention <- inputdata$state_distribution_intervention * inputdata$state_QALYs
+      total_QALYs_intervention <- QALYs_intervention[1]+QALYs_intervention[2]+QALYs_intervention[3]
+      
+      costs_control <- inputdata$state_distribution_control * inputdata$state_costs_control
+      total_cost_control <- costs_control[1]+costs_control[2]+costs_control[3]
+      QALYs_control <- inputdata$state_distribution_control * inputdata$state_QALYs
+      total_QALYs_control <- QALYs_control[1]+QALYs_control[2]+QALYs_control[3]
+      
+      ICER <- (total_cost_intervention - total_cost_control) / (total_QALYs_intervention - total_QALYs_control)
+      
+      if(CET >= ICER){
+        CEspace$BAU_Cost_Vector[i] <- inputdata$state_costs_control[1]
+        CEspace$Int_Cost_vector[i] <- inputdata$state_costs_intervention[1]
+        CEspace$Helth_QALY_Vector[i] <- inputdata$state_QALYs[1]
+        CEspace$Sick_QALY_Vector[i] <- inputdata$state_QALYs[2] 
+      } 
+      rm(i)
+    }
+    
+    Cost_Effective_Points <- ggplot(CEspace, aes(x=CEspace[[input$xparam]], y=CEspace[[input$yparam]]))+
+      geom_point()+
+      xlab(input$xparam)+
+      ylab(input$yparam)+
+      ggtitle("Cost-Effective Plane")
+    
+    return(Cost_Effective_Points)
+  }
+  
+ # tornado = function(inputdata){
+ #   
+ # }
+  
   output$ICER <- renderText({
-    ICER_rand("inputdata.csv")
+    ICER_rand(inputdata)
     })
   
   output$ICER_determ <- renderText ({
-    ICER_determ("inputdata.csv")
+    ICER_determ(inputdata)
   })
   
   output$CEAC <- renderPlot ({
-    CEAC("inputdata.csv")
+    CEAC(inputdata)
   })
   
   output$portion_effective <- renderText ({
-    portion_effective("inputdata.csv")
+    portion_effective(inputdata)
   })
+  
+  output$plane <- renderPlot({
+    cost_effective(inputdata)
+  })
+  
+ # output$tornado <- renderPlot({
+ #   tornado(inputdata)
+ # })
 }
 
 shinyApp(ui, server)
